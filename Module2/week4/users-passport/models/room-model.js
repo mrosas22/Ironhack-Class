ebcd9c08a9cbdd12e4854a92e1e9d3d9f3254ccf
mrosas22 <miller.rosas22@gmail.com>
2019-02-11 14:18:59 -0500

    Add file upload and rooms routes

--- Module2/week4/users-passport/models/room-model.js
+++ Module2/week4/users-passport/models/room-model.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const roomSchema = new Schema({
-  name :       {type: String},
-  description: {type: String}, 
-  imageUrl:    {type: String},
-  owner:       {type: Schema.Types.ObjectId, ref: 'User'}
-  },{
-    timestamps: true
+  name: { type: String },
+  description: { type: String },
+  imageUrl: { type: String },
+  owner: { type: Schema.Types.ObjectId, ref: 'User' }
 })
 
 const Room = mongoose.model('Room', roomSchema);
-
 module.exports = Room;
--- Module2/week4/users-passport/routes/room-routes.js
+++ Module2/week4/users-passport/routes/room-routes.js
@@ -6,10 +6,15 @@ const User = require('../models/user-model');
 
 const fileUploader = require('../config/upload-setup/cloudinary');
 
-
+// GET route to display the form to create a room
 router.get('/rooms/add', isLoggedIn, (req, res, next) => {
   res.render('room-pages/add-room');
 });
+
+
+
+// POST route to create the room -> has the image uploading example 🥳
+
                     //  <input type="file" name="imageUrl" id="">
 //                                                  |
 router.post('/create-room', fileUploader.single('imageUrl'),(req, res, next) => {
@@ -24,25 +29,32 @@ router.post('/create-room', fileUploader.single('imageUrl'),(req, res, next) => 
     owner: req.user._id
   })
   .then( newRoom => {
-    console.log('room created: ', newRoom)
+    // console.log('room created: ', newRoom)
     res.redirect('/rooms');
   } )
   .catch( err => next(err) )
 })
 
-router.get('/rooms', (req, res, next) =>{
+router.get('/rooms', isLoggedIn, (req, res, next) => {
   Room.find()
-    .then(roomsFromDB =>{
-      res.render('room-pages/room-list', {roomsFromDB})
+  .then(roomsFromDB => {
+    roomsFromDB.forEach(oneRoom => {
+      // each room has the 'owner' property which is user's id
+      // if owner (the id of the user who created a room) is the same as the currently logged in user
+      // then create additional property in the oneRoom object (maybe isOwner is not the best one but ... 🤯)
+      // and that will help you to allow that currently logged in user can edit and delete only the rooms they created
+
+      if(oneRoom.owner.equals(req.user._id)){
+        oneRoom.isOwner = true;
+      }
     })
+    res.render('room-pages/room-list', { roomsFromDB })
+  })
 })
 
 
-
-
-
-
-
+// this is the function we use to make sure the route and the functionality is 
+// available only if we have user in the session
 function isLoggedIn(req, res, next){
   if(req.user){
     next();
