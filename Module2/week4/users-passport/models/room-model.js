74ae74205286db26c3d7549a0f3d8279910c3fcd
mrosas22 <miller.rosas22@gmail.com>
2019-02-13 00:03:29 -0500

    Added edit and delete routes

--- Module2/week4/users-passport/models/room-model.js
+++ Module2/week4/users-passport/models/room-model.js
@@ -5,7 +5,9 @@ const roomSchema = new Schema({
   name: { type: String },
   description: { type: String },
   imageUrl: { type: String },
-  owner: { type: Schema.Types.ObjectId, ref: 'User' }
+  owner: { type: Schema.Types.ObjectId, ref: 'User' },
+  //The ref option is what tells Mongoose which model to use during population
+  reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}]//<===in our case the Review model
 })
 
 const Room = mongoose.model('Room', roomSchema);
--- Module2/week4/users-passport/routes/room-routes.js
+++ Module2/week4/users-passport/routes/room-routes.js
@@ -3,6 +3,7 @@ const router  = express.Router();
 
 const Room = require('../models/room-model');
 const User = require('../models/user-model');
+const Review = require('../models/review-model');
 
 const fileUploader = require('../config/upload-setup/cloudinary');
 
@@ -18,9 +19,6 @@ router.get('/rooms/add', isLoggedIn, (req, res, next) => {
                     //  <input type="file" name="imageUrl" id="">
 //                                                  |
 router.post('/create-room', fileUploader.single('imageUrl'),(req, res, next) => {
-  // console.log('body: ', req.body);
-  // console.log(' - - -- - -- - -- - - -- - - ');
-  // console.log('file: ', req.file);
   const { name, description } = req.body;
   Room.create({
     name,
@@ -35,23 +33,100 @@ router.post('/create-room', fileUploader.single('imageUrl'),(req, res, next) => 
   .catch( err => next(err) )
 })
 
-router.get('/rooms', isLoggedIn, (req, res, next) => {
-  Room.find()
+router.get('/rooms', (req, res, next) => {
+  Room.find().populate('owner')
   .then(roomsFromDB => {
     roomsFromDB.forEach(oneRoom => {
-      // each room has the 'owner' property which is user's id
-      // if owner (the id of the user who created a room) is the same as the currently logged in user
-      // then create additional property in the oneRoom object (maybe isOwner is not the best one but ... 🤯)
-      // and that will help you to allow that currently logged in user can edit and delete only the rooms they created
-
-      if(oneRoom.owner.equals(req.user._id)){
-        oneRoom.isOwner = true;
+      // if there's a user in a session:
+      if(req.user && oneRoom.owner){
+        if(oneRoom.owner.equals(req.user._id)){
+          oneRoom.isOwner = true;
+        }
       }
     })
     res.render('room-pages/room-list', { roomsFromDB })
   })
 })
 
+
+//GET route to display details of rooms
+router.get('/rooms/:id', (req, res, next) =>{
+  const roomId = req.params.id;
+  Room.findById(roomId).populate('owner')
+    .then(theRoom => {
+      console.log('the current user id: ', req.user)
+      console.log('the room owner id is: ', theRoom.owner)
+      if(req.user && theRoom.owner){
+        if(theRoom.owner.equals(req.user._id)){
+          theRoom.isOwner = true;
+        }
+      }
+      res.render('room-pages/room-details', {theRoom})
+    })
+    .catch(err => console.log('Error while getting the Book: ', err))
+})
+
+//Create Reviews
+// router.post('/rooms/reviews/add', (req, res, next) =>{
+//   const 
+//   Room.findById(req.params.id, (err, room) =>{
+//     if (err) throw new Error(err);
+//     const newPost = {
+//       user: req.user._id,
+//       comment: req.body.comment
+//     };
+//     Review.create(newPost, (err, post) =>{
+//       if (err){
+//         res.redirect('/rooms');
+//         throw new Error (err)
+//       }
+//       room.reviews.push(newPost);
+//       room.save((err) =>{
+//         return res.redirect('/rooms')
+//       })
+//     })
+//   })
+// })
+
+//Delete Route
+//localhost:3000/rooms/5c621e706840861a4ad8f2b9/delete?
+router.post('/rooms/:theRoomId/delete', (req, res, next) =>{
+  Room.findById(req.params.theRoomId)
+    .then(theRoom => {
+      
+      res.redirect('/rooms')
+    })
+    .catch(err => console.log('Error while deleting the Room: ', err))
+})
+
+
+//GET routes to edit rooms
+//localhost:3000/rooms/5c61c60a9348bc08c738c7d0/edit
+router.get('/rooms/:id/edit', (req, res, next) =>{
+  Room.findById(req.params.id)
+    .then(foundRoom =>{
+          res.render('room-pages/edit-room', {room: foundRoom});
+    })
+    .catch(err => ('Error while editing room: ', err))
+})
+
+//post route to update changes
+//localhost:3000/rooms/5c61c60a9348bc08c738c7d0/update
+router.post('/rooms/:id/update', fileUploader.single('imageUrl'), (req, res, next) =>{
+  // console.log('Updates are: ', req.body)
+  Room.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    description : req.body.description,
+    imageUrl: req.file.secure_url,
+    owner: req.user._id
+  })
+    .then(updatedRoom => {
+      console.log('Is this updated: ', updatedRoom)
+      res.redirect(`/rooms/${req.params.id}`)
+      // res.redirect('/rooms')
+    })
+    .catch(err => console.log('Error while saving the updates in DB: ', err))
+})
 
 // this is the function we use to make sure the route and the functionality is 
 // available only if we have user in the session
@@ -63,5 +138,8 @@ function isLoggedIn(req, res, next){
   }
 
 }
+
+//Get the details
+
 
 module.exports = router;
