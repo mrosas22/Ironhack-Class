5d740004b8d189212958f4f94488f7002e4c1a29
mrosas22 <miller.rosas22@gmail.com>
2019-04-12 23:46:13 -0400

    Added file-example client

--- Module3/Week4/file-upload-example-server-master/routes/file-upload-routes.js
+++ Module3/Week4/file-upload-example-server-master/routes/file-upload-routes.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router  = express.Router();
-
 // include CLOUDINARY:
 const uploader = require('../configs/cloudinary-setup');
-
 router.post('/upload', uploader.single("imageUrl"), (req, res, next) => {
     // console.log('file is: ', req.file)
-
-    if (!req.file) {
+if (!req.file) {
       next(new Error('No file uploaded!'));
       return;
     }
@@ -15,5 +12,4 @@ router.post('/upload', uploader.single("imageUrl"), (req, res, next) => {
     // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
     res.json({ secure_url: req.file.secure_url });
 })
-
 module.exports = router;
