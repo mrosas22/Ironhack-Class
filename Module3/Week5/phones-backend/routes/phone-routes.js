60f435b2c3d42f3d9dcb71839c5d3215b3fdf399
mrosas22 <miller.rosas22@gmail.com>
2019-05-20 23:53:50 -0400

    Modified phones project backend

--- Module3/Week5/phones-backend/routes/phone-routes.js
+++ Module3/Week5/phones-backend/routes/phone-routes.js
@@ -3,20 +3,20 @@ const router = express.Router();
 
 const Phone = require('../models/phone-model');
 
-router.post('/phones', (req, res, next) =>{
-    const {brand, model, price, image, spec} = req.body;
+router.post('/phone', (req, res, next) =>{
+    const {brand, model, price, image, specs} = req.body;
     if(brand == '' || model == '' || price === '', image == '', specs == ''){
         // send error JSON if any of the fields is empty or password doesn't contain a number
         res.status(401).json({ message: "All fields need to be filled." })
         return;
       }
-    Phone.create({brand, model, price, image, spec})
+    Phone.create({brand, model, price, image, specs})
         .then(phoneDoc => res.json(phoneDoc))
         .catch(err => next(err));
 });
 
 //get the list of all phones from DB
-router.get('/phones', (req,res, next) =>{
+router.get('/phone', (req,res, next) =>{
     Phone.find()
     .sort({createAt: -1})
     .limit(10)
